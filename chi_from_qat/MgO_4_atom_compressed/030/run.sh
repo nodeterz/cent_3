c2962ecdfd81cb52c7488a0ceb879f9a2e40de0b
nodeterz <[rahmatizad@gmail.com]>
2019-10-21 15:31:12 +0330

    Merged with gavazang

--- chi_from_qat/MgO_4_atom_compressed/030/run.sh
+++
@@ -1,4 +0,0 @@
-gfortran -g -C -fbacktrace V2.F90 -L/opt/intel/composer_xe_2013.2.146/mkl/lib/intel64 -lmkl_rt -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -o cent_2.out
-./cent_2.out
-gfortran -g -C -fbacktrace cent_1.F90 -L/opt/intel/composer_xe_2013.2.146/mkl/lib/intel64 -lmkl_rt -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -o cent_1.out
-./cent_1.out
